09f6c2b9d7b6e02365aba3cb0476f171c3ca5342
JoffreyY <Joffrey.yvon@icloud.com>
2016-09-24 18:27:30 +0200

    bugg correction

--- training_php/model/forum/get_articles.php
+++ training_php/model/forum/get_articles.php
@@ -1,12 +1,12 @@
 <?php
-function get_article($offset, $limit)
+function get_articles($offset, $limit)
 {
     global $bdd;
     $offset = (int) $offset;
     $limit = (int) $limit;
 
-    $req = $bdd->prepare('SELECT id, title, content, author DATE_FORMAT(add_date, \'%d/%m/%Y � %Hh%imin%ss\')
-    AS add_date_fr FROM ticket ORDER BY date_creation DESC LIMIT :offset, :limit');
+    $req = $bdd->prepare('SELECT id, title, content, author, DATE_FORMAT(add_date, \'%d/%m/%Y � %Hh%imin%ss\')
+    AS add_date_fr FROM article ORDER BY date_creation DESC LIMIT :offset, :limit');
     $req->bindParam(':offset', $offset, PDO::PARAM_INT);
     $req->bindParam(':limit', $limit, PDO::PARAM_INT);
     $req->execute();
