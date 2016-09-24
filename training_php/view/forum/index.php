fdf8c34ec16991dc4c3b9ed4619a1d8b38b247ce
JoffreyY <Joffrey.yvon@icloud.com>
2016-09-24 18:20:07 +0200

    index page in view

---
+++ training_php/view/forum/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Forum</title>
+        <meta content="text/html" charset="utf-8" />
+        <link href="view/forum/stylesheet.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <h1>Forum</h1>
+        <p>Derniers articles du forum :</p>
+
+<?php
+foreach($articles as $article)
+{
+?>
+<div class="articles">
+    <h3 class="title">
+        <?= $article['title']; ?> par <?= $article['author']?>, <em>le <?= $article['add_date_fr']; ?></em>
+    </h3>
+
+    <p class='content'>
+    <?= $article['content']; ?>
+    <br />
+    <em><a href="commentaires.php?billet=<?= $article['id']; ?>">Commentaires</a></em>
+    </p>
+</div>
+<?php
+}
+?>
+</body>
+</html>
